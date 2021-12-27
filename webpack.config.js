6d57cbdb4d4180656bb962d77c6e58a7f4c031bf
ChenChin343 <armandocrescencio343@gmail.com>
2021-12-27 16:20:29 -0600

    añadi configuracion del webPck

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+  entry: './src/index.js',
+  devServer: { 
+      static: "./dist",
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
